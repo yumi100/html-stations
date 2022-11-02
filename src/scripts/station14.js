4783511a2b662d8a304a0ad3e0387e27d8aed991
yumi100 <yyummik@gmail.com>
2022-11-03 00:10:15 +0900

    clear

--- src/scripts/station14.js
+++ src/scripts/station14.js
@@ -2,17 +2,21 @@ function getData() {
     // これから作ろうとしているもので、JavaScriptでフルネームを生成する必要が出てきました。
     // ただ現状持っている情報では、名前と苗字をそれぞれのみしかありません。
     // そのため、JavaScriptで連結させて生成することにしました。
-    const test = [
-        { id: 1, first_name: '優', family_name: '大木', affilication: 'TechTrain', is_student: false },
-        { id: 2, first_name: '太郎', family_name: '山田', affilication: 'HogeHoge大学', is_student: true }
-    ];
-    return test.map(buildFullName)
+        const test = [
+            { id: 1, first_name: '優', family_name: '大木', affilication: 'TechTrain', is_student: false },
+            { id: 2, first_name: '太郎', family_name: '山田', affilication: 'HogeHoge大学', is_student: true }
+        ];
+    return test.map(buildFullName);
 }
 
 function buildFullName(data) {
     // Station14の問題はこちらです。想定する答えをここを書いてください。
     // 氏名がわかるようにしつつ、半角スペースで繋いでください。
     // またtest配列のそれぞれのオブジェクトに対して、full_nameのプロパティが追加されるように実装すること
+
+    data.full_name = data.family_name + ' ' + data.first_name;
+    //console.log(data)→これで確認
+    return data
 }
 
 // 出力が想定される答え
@@ -20,3 +24,15 @@ function buildFullName(data) {
 //     {id: 1, full_name: '大木 優', first_name: '優', family_name: '大木',affilication: 'TechTrain', is_student: false},
 //     {id: 2, full_name: '山田 太郎', first_name: '太郎', family_name: '山田',affilication: 'HogeHoge大学', is_student: true}
 // ];
+// 
+// 他の人の別解
+// var name = data.family_name + ' ' + data.first_name;
+// let result = new Object();
+// result.id = data.id;
+// result.full_name = name;
+// result.first_name = data.first_name;
+// result.family_name = data.family_name;
+// result.affilication = data.affilication;
+// result.is_student = data.is_student;
+//
+// return result;   →これだと想定される答え通りの順番になる。結果は変わらないので短い方にした。
--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -6,11 +6,34 @@ async function getData() {
     { id: 1, first_name: '優', family_name: '大木', affilication: 'TechTrain', is_student: false },
     { id: 2, first_name: '太郎', family_name: '山田', affilication: 'HogeHoge大学', is_student: true }
   ];
-  const result = await test()
+  const result = await test(userList)
   return await result
 }
 
-function test() {
-  return
+function test(arr) {
+  sec = 3;
+  const userList_map = arr.map(data => {
+    data.full_name = data.family_name + ' ' + data.first_name;
+    return data
+  });
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log(`wait: ${sec} sec`);
+      resolve(userList_map);
+    }, sec * 1000);
+  });
 }
 
+// 参考コード
+//function sleep(sec) {
+//return new Promise((resolve) => {
+//  setTimeout(() => {
+//      console.log(`wait: ${sec} sec`);
+//      resolve(sec);
+//  }, sec * 1000);
+//});
+//}
+//const userList_map = userList.map(data => {
+//  data.full_name = data.family_name + ' ' + data.first_name;
+//  return data
+//})
